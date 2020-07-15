116a5b75b99456538cf60be0a279898f15f44633
Peggy Wollenhaupt <isande@gmail.com>
2020-07-15 11:50:48 -0600

    initial commit

---
+++ I Am Rich Swift UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich Swift UI
+//
+//  Created by Peggy Wollenhaupt on 7/15/20.
+//  Copyright © 2020 Peggy Wollenhaupt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
